ff8bc3314649be3df5507caa19c1876ab5b002ee
welborn <djidjonouaflou@gmail.com>
2025-09-18 16:28:12 +0000

    Refactor authentication service and controller to support email-based OTP requests and verifications

--- src/features/auth/Auth.model.ts
+++ src/features/auth/Auth.model.ts
@@ -1,3 +1,35 @@
+// export type UserRole = "patient" | "psychologue" | "admin";
+
+// export interface IUser {
+//   id?: string;                // UUID
+//   pseudonyme?: string;        // VARCHAR(50), unique (optionnel pour psy/admin)
+//   motDePasse: string;        // VARCHAR
+//   email?: string | null;     // Hash/crypt email patient (string)
+//   email_clair?: string | null; // VARCHAR(100) (pour psy/admin)
+//   dateCreation: Date;        // TIMESTAMP DEFAULT now()
+//   role: UserRole;            // patient | psychologue | admin
+//   nom?: string | null; // VARCHAR(100), optionnel (par ex. pour psy/admin)
+//   prenom?: string | null; // VARCHAR(100), optionnel (par ex. pour psy/admin)
+// }
+
+// export interface ITherapistProfile {
+//   userId: string; // FK vers IUser.id
+//   domaines: string[]; 
+//   sujets: string[];
+//   methodes: string[];
+//   description: string;
+//   cvUrl?: string; // lien vers le CV stocké
+//   motivation: string;
+// }
+// export interface IOTP {
+//   id?: number;
+//   userId: string;
+//   otp: string;
+//   type: "activation" | "reset";
+//   expiresAt: Date;
+//   used?: boolean;
+//   createdAt?: Date;
+// }
 export type UserRole = "patient" | "psychologue" | "admin";
 
 export interface IUser {
